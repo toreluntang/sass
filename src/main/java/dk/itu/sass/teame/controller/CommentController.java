98d957f7248a251d83d633e26a64feba180e42c8
toreluntang <toreluntang@gmail.com>
2016-03-22 16:38:02 +0100

    quick fix

--- src/main/java/dk/itu/sass/teame/controller/CommentController.java
+++ src/main/java/dk/itu/sass/teame/controller/CommentController.java
@@ -1,12 +1,14 @@
 package dk.itu.sass.teame.controller;
 import java.time.LocalDateTime;
 
+import dk.itu.sass.teame.entity.Comment;
+
+
 public class CommentController {
 
 	public Comment addComment(String body, int userid, int imageid) {
-		String timestamp = String.now().toString();
+		String timestamp = LocalDateTime.now().toString();
 
-
-//
+		return null;
 	}
 }
