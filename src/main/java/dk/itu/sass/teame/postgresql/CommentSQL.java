500fd32687e1c38c50d31a0cbb047f13199c8a5a
toreluntang <toreluntang@gmail.com>
2016-04-04 19:16:57 +0200

    comment

--- src/main/java/dk/itu/sass/teame/postgresql/CommentSQL.java
+++ src/main/java/dk/itu/sass/teame/postgresql/CommentSQL.java
@@ -15,17 +15,10 @@ import dk.itu.sass.teame.entity.Account;
 import dk.itu.sass.teame.entity.Comment;
 
 public class CommentSQL {
-	private String propertyFilePath = "src/main/resources/postgresql.properties";
-	private String url;
-	private String dbusername;
-	private String dbpassword;
 	
 	public CommentSQL(){
 		
 		try{
-			url = readProperty("postgresurl");
-			dbusername = readProperty("postgresuser");
-			dbpassword = readProperty("postgrespass");
 			Class.forName("org.postgresql.Driver");
 		}
 		catch(Exception e) {e.printStackTrace();}
@@ -37,7 +30,7 @@ public class CommentSQL {
 		
 		try {
 
-			try (Connection con = DriverManager.getConnection(url, dbusername, dbpassword)) {
+			try (Connection con = DriverManager.getConnection("jdbc:postgresql://horton.elephantsql.com:5432/hmdgzyax", "hmdgzyax", "8ETS72wV53uGfPIs-RCJy_tolfPs481n")) {
 
 				PreparedStatement pre = null;
 				String stm = "select commentid, body, userid, timestamp, imageid from comment where imageid = ?";
@@ -70,12 +63,7 @@ public class CommentSQL {
 		long bob = -1; // confuse the enemy with nice variable names! 
 
 		try {
-			String url = readProperty("postgresurl");
-			String dbusername = readProperty("postgresuser");
-			String dbpassword = readProperty("postgrespass");
-			Class.forName("org.postgresql.Driver");
-
-			try (Connection con = DriverManager.getConnection(url, dbusername, dbpassword)) {
+			try (Connection con = DriverManager.getConnection("jdbc:postgresql://horton.elephantsql.com:5432/hmdgzyax", "hmdgzyax", "8ETS72wV53uGfPIs-RCJy_tolfPs481n")) {
 				PreparedStatement pre = null;
 				String stm = "insert into comment(body, userid, timestamp, imageid) VALUES(?, ?, ?, ?)";
 				pre = con.prepareStatement(stm,Statement.RETURN_GENERATED_KEYS);
@@ -100,18 +88,4 @@ public class CommentSQL {
 
 		return bob;
 	}
-	
-	
-	 private String readProperty(String key) throws Exception {
-
-	 	Properties prop = new Properties();
-	 	String res = "";
-
-	 	try (InputStream input = new FileInputStream(propertyFilePath)) {
-	 		prop.load(input);
-	 		res = prop.getProperty(key);
-	 	}
-	 	System.out.println("I HAVE READ THE FOLLOWING : "+key+"="+res);
-	 	return res;
-	 }
 }
